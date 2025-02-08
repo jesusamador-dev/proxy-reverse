863aba5bdb6048eb8e0c0c93195ae2560d4bf64b
Jesús Guadalupe Macías Amador <47731561+jesusamador-dev@users.noreply.github.com>
2025-02-08 05:22:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -36,32 +36,7 @@ async def consume_external_service(path: str, request: Request):
         raise HTTPException(status_code=500, detail=f"Error inesperado: {str(e)}")
 
 
-@app.get("/mayra")
-async def consume_endpoint(request: Request):  # Elimina el parámetro 'path' innecesario
-    try:
-        base_url = 'https://api.odysseyaimodel.com/mapa/gmaps'
-        params = {  # Define los parámetros en un diccionario
-            "y": 3632,
-            "x": 1800,
-            "zoom": 13,
-            "layers": "MapaBAZ"
-        }
-
-        try:
-            req = requests.get(base_url, params=params, timeout=10, stream=True)
-            req.raise_for_status()
-
-            content = b"".join(chunk for chunk in req.iter_content(chunk_size=8192))
-
-            return Response(
-                content=content,  # Ahora 'content' son bytes
-                status_code=req.status_code,
-                media_type=req.headers.get("Content-Type")
-            )
-        except requests.exceptions.RequestException as e:
-            raise HTTPException(status_code=500, detail=f"Error al comunicarse con el servicio externo: {str(e)}")
-
-    except HTTPException as e:
-        raise
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error inesperado: {str(e)}")
+# Punto de entrada
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))  # Railway asigna un puerto automáticamente
+    uvicorn.run(app, host="0.0.0.0", port=port)
